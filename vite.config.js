5cb2c55eb5b84d585d2629909616954974099a22
rajaman00 <raj249919@gmail.com>
2024-11-15 11:52:54 +0530

    15-11-2024

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', /* other common libraries */],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // adjust as needed
-  },
-  base: '/bsptcl-edited-8-9/',
+  
 })
